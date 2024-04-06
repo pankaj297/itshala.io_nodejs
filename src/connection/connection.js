c9d45ebe26cbd453c7f92d765189bf5132c15bdc
pankajnaik <115211948+pankaj297@users.noreply.github.com>
2024-04-06 18:32:54 +0530

    some change

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-const DB = process.env.MONGOURL;
+// const DB = process.env.MONGOURL;
+
+const DB ="mongodb+srv://pankajnaik958:itshala01@cluster1.wl2qu8d.mongodb.net/ItshalaProject";
 
 mongoose
   .connect(`${DB}ItshalaProject`, {})
